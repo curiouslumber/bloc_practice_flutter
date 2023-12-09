60854a13a6e9344e6e78dd74f62946b89877c16d
Noel Pinto <noelpinto47@gmail.com>
2023-12-10 02:04:09 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:timer_bloc_project/src/features/counter/bloc/counter_bloc.dart';
 import 'package:timer_bloc_project/src/utils/colors.dart';
-import 'package:timer_bloc_project/src/utils/strings.dart';
 import 'package:timer_bloc_project/src/views/home/homepage.dart';
 import 'package:timer_bloc_project/src/routing/router.dart' as app_router;
 
@@ -18,13 +17,12 @@ class MyApp extends StatelessWidget {
     return BlocProvider(
       create: (BuildContext context) => CounterBloc(),
       child: MaterialApp(
-        title: 'Flutter Demo',
+        title: 'BLoC App',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primarySwatch),
           useMaterial3: true,
         ),
         home: const HomePage(),
-        initialRoute: StringConstant.routeHome,
         onGenerateRoute: app_router.Router.generateRoute,
       ),
     );
